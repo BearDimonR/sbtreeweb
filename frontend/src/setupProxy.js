6018872fb58b65f0759f5997b4475d715a1b0ad3
BeardimonR <beardimon2001@gmail.com>
2022-02-08 20:46:51 +0200

    add proxy

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+require("dotenv").config();
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: process.env.REACT_APP_PROXY_URL,
+      changeOrigin: true,
+    })
+  );
+};
